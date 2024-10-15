f547464b461f4ee323f0b5534799ff63b185cfdc
Lucas Liu <lucasqhliu@gmail.com>
2024-10-15 12:06:12 -0700

    added log in process

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,14 @@ function sayHello() {
     //do something
 }
 //sayHello();    //invoke function
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
